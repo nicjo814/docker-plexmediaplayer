f6629da7e4adf1155dcf50197f798d8c504a625f
Niclas Berglind <nb@kjam.se>
2016-02-25 13:27:59 +0100

    Add support for geometry

--- init/40_plex.sh
+++ init/40_plex.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+mkdir -p /root/.local/share/plexmediaplayer
+
 if [ -f "/root/.local/share/plexmediaplayer/machine-id" ]; then
     cp /root/.local/share/plexmediaplayer/machine-id /etc/machine-id
 else
-    dbus-uuidgen > /root/.local/share/plexmediaplayer/machine-id
-    cp /root/.local/share/plexmediaplayer/machine-id /etc/machine-id
+	dbus-uuidgen > /root/.local/share/plexmediaplayer/machine-id
+	cp /root/.local/share/plexmediaplayer/machine-id /etc/machine-id
 fi
 
-/usr/local/bin/plexmediaplayer
+if [ -z $GEOMETRY ]; then
+	/usr/local/bin/plexmediaplayer -geometry $GEOMETRY
+else
+	/usr/local/bin/plexmediaplayer
+fi
