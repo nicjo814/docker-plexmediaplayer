3226c6f0054fd18f774452b1075f549c9e86cf2a
Niclas Berglind <nb@kjam.se>
2016-02-26 19:04:57 +0100

    Update

--- init/40_plex.sh
+++ init/40_plex.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-mkdir -p /root/.local/share/plexmediaplayer
+#mkdir -p /root/.local/share/plexmediaplayer
 
-if [ -f "/root/.local/share/plexmediaplayer/machine-id" ]; then
-    cp /root/.local/share/plexmediaplayer/machine-id /etc/machine-id
-else
-	dbus-uuidgen > /root/.local/share/plexmediaplayer/machine-id
-	cp /root/.local/share/plexmediaplayer/machine-id /etc/machine-id
-fi
+#if [ -f "/root/.local/share/plexmediaplayer/machine-id" ]; then
+#    cp /root/.local/share/plexmediaplayer/machine-id /etc/machine-id
+#else
+#	dbus-uuidgen > /root/.local/share/plexmediaplayer/machine-id
+#	cp /root/.local/share/plexmediaplayer/machine-id /etc/machine-id
+#fi
 
-if [ -z $GEOMETRY ]; then
-	/usr/local/bin/plexmediaplayer -geometry $GEOMETRY
-else
-	/usr/local/bin/plexmediaplayer
-fi
+#if [ -z $GEOMETRY ]; then
+#	/usr/local/bin/plexmediaplayer -geometry $GEOMETRY
+#else
+/usr/local/bin/plexmediaplayer
+#fi
