1fcf96910f06c9889e9b40375e35fd335c709989
Niclas Berglind <nb@kjam.se>
2016-02-29 07:03:26 +0100

    Cleaned up Plex init file

--- init/40_plex.sh
+++ init/40_plex.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-#mkdir -p /root/.local/share/plexmediaplayer
-
-#if [ -f "/root/.local/share/plexmediaplayer/machine-id" ]; then
-#    cp /root/.local/share/plexmediaplayer/machine-id /etc/machine-id
-#else
-#	dbus-uuidgen > /root/.local/share/plexmediaplayer/machine-id
-#	cp /root/.local/share/plexmediaplayer/machine-id /etc/machine-id
-#fi
-
-#if [ -z $GEOMETRY ]; then
-#	/usr/local/bin/plexmediaplayer -geometry $GEOMETRY
-#else
-#/usr/local/bin/plexmediaplayer
-#fi
+#TODO Start PMP automatically
+#plexmediaplayer
